9c243efbff922fcb8e4e57ae2d6efc29a94846b6
Noel Röhrig <32901324+idmedia-roehrig@users.noreply.github.com>
2019-12-12 10:02:32 +0100

    include player order

--- code/main.py
+++ code/main.py
@@ -1,10 +1,15 @@
 import random
 
-bodyParts = list(input("Enter the body parts (separated with a ';' character): ").split(";"))
-players = list(input("Enter the players (separated with a ';' character): ").split(";"))
+#bodyParts = list(input("Enter the body parts (separated with a ';' character): ").split(";"))
+#players = list(input("Enter the players (separated with a ';' character): ").split(";"))
+bodyParts = ["Arm", "Bein", "Fuß"]
+players = ["Gustav", "Donald", "Dagobert"]
 
-sourceBodypart = bodyParts[random.randrange(len(bodyParts) - 1)]
-personToTouch = players[random.randrange(len(bodyParts) - 1)]
-targetBodypart = bodyParts[random.randrange(len(bodyParts) - 1)]
-
-print(f"The next player in line needs to touch {personToTouch}'s {targetBodypart} with their {sourceBodypart}" )
+while True:
+    for player in players:
+        sourceBodypart = bodyParts[random.randrange(len(bodyParts) - 1)]
+        personToTouch = players[random.randrange(len(bodyParts) - 1)]
+        targetBodypart = bodyParts[random.randrange(len(bodyParts) - 1)]
+        print(f"The next player {player} needs to touch {personToTouch}'s {targetBodypart} with their {sourceBodypart}" )
+    if(input("Another round? y/n ") == "n"):
+        break
